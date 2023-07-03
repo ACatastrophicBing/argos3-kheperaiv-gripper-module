f564072b6551bb6f32ec20bc6fec564da11799b2
Chandler Garcia <crgarcia@wpi.edu>
2023-07-03 12:45:27 -0400

    Testing and trying to figure out force sensing

--- src/plugins/robots/kheperaiv/simulator/dynamics2d_kheperaiv_model.cpp
+++ src/plugins/robots/kheperaiv/simulator/dynamics2d_kheperaiv_model.cpp
@@ -407,14 +407,13 @@ namespace argos {
             CRadians(m_ptActualBaseBody->a)),
          CVector3::Z);
       printf("Turret Location is (%f, %f)\n",m_ptActualGripperBody->p.x,m_ptActualGripperBody->p.y);
-      printf("Gripper Location is (%f, %f)\n",m_cGripperEntity);
+      // printf("Gripper Location is (%f, %f)\n",m_cGripperEntity);
       if(m_pcGripper->GetConstraint()){
          cpDampedSpring* ptSpring = reinterpret_cast<cpDampedSpring*>(m_pcGripper->GetConstraint());
          printf("restLength = %.3f\n", ptSpring->restLength);
          printf("anchor1 = %.3f,%.3f\n", ptSpring->anchr1.x,ptSpring->anchr1.y);
          printf("anchor2 = %.3f,%.3f\n", ptSpring->anchr2.x,ptSpring->anchr2.y);
-         printf("\n");
-
+         // printf("anchor1 cpDampedSpring force = %.4f\n", ptSpring->springForceFunc);
       }
    }
 
